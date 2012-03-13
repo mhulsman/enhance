ea22dce3075b438316744f1efa4ecbacf5af03de
Marc Hulsman <m.hulsman@tudelft.nl>
2012-03-13 13:25:29 +0100

    Add gcc as dependency for qt (compile fails on gcc-4.1)

--- packages/qt-4.8.0.py
+++ packages/qt-4.8.0.py
@@ -1,5 +1,5 @@
 from package import *
 
 class qt(MakePackage):
-    dependencies=['xorg']
+    dependencies=['gcc','xorg']
     fetch="http://get.qt.nokia.com/qt/source/qt-everywhere-opensource-src-%(version)s.tar.gz"
