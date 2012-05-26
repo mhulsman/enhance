0587c6c587fdbfa6ff7da8cb465d7c1f5ee0572b
Marc Hulsman <m.hulsman@tudelft.nl>
2012-05-26 09:24:29 +0200

    git dep

--- packages/git-1.7.9.3.py
+++ packages/git-1.7.9.3.py
@@ -1,7 +1,7 @@
 from package import *
 
 class git(Package):
-    dependencies = ["python",'openssl','zlib']
+    dependencies = ["python",'openssl','zlib','curl']
 
     fetch="http://git-core.googlecode.com/files/git-1.7.9.3.tar.gz"
 
