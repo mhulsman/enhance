76f60df2d0445fc07496b6c14226fb7c775105ac
Marc Hulsman <m.hulsman@tudelft.nl>
2012-05-26 10:11:26 +0200

    e2fsprogs configure options

--- packages/e2fsprogs-1.42.3.py
+++ packages/e2fsprogs-1.42.3.py
@@ -1,4 +1,5 @@
 from package import *
 class e2fsprogs(MakePackage):
     fetch='http://kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.42.3/e2fsprogs-1.42.3.tar.gz'
-
+    config='./configure --prefix=%(prefix)s --enable-elf-shlibs'
+    install="make install && make install-libs"
