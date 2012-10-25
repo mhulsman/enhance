039687c9a79bacc85ced0fbfae5a61d6f966c6e8
Marc Hulsman <m.hulsman@tudelft.nl>
2012-10-25 10:50:13 +0200

    mpfr -> 3.1.1

--- packages/mpfr-3.1.1.py
+++ packages/mpfr-3.1.1.py
@@ -1,5 +1,5 @@
 from package import *
 class mpfr(MakePackage):
     dependencies=['gmp']
-    fetch='http://www.mpfr.org/mpfr-current/mpfr-3.1.0.tar.gz'
+    fetch='http://www.mpfr.org/mpfr-current/mpfr-%(version)s.tar.gz'
 
