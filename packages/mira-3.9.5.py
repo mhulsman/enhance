b21ae10f2fa1715c4eb34abe7024da2a2daff958
Marc Hulsman <m.hulsman@tudelft.nl>
2012-11-15 14:23:10 +0100

    mira

---
+++ packages/mira-3.9.5.py
@@ -0,0 +1,7 @@
+from package import *
+
+class mira(MakePackage):
+    dependencies = ['boost','gperftools','flex']
+    fetch="http://downloads.sourceforge.net/project/mira-assembler/MIRA/development/mira-3.9.5.tar.gz"
+
+
