674a6729a973cc805b6208991f2558a62c442398
Marc Hulsman <m.hulsman@tudelft.nl>
2012-11-15 14:14:44 +0100

    libunwind

--- packages/gperftools-2.0.py
+++ packages/gperftools-2.0.py
@@ -1,6 +1,7 @@
 from package import *
 
 class gperftools(MakePackage):
+    dependencies = ['libunwind']
     fetch="http://gperftools.googlecode.com/files/gperftools-2.0.tar.gz"
 
 
---
+++ packages/libunwind-1.1.py
@@ -0,0 +1,6 @@
+from package import *
+
+class libunwind(MakePackage):
+    fetch="http://download.savannah.gnu.org/releases/libunwind/libunwind-1.1.tar.gz"
+
+
