941feb810e8bd7c5509e204492fc601cf8c0eb9a
Marc Hulsman <m.hulsman@tudelft.nl>
2012-12-14 13:28:12 +0100

    Genome comparison tools

---
+++ packages/ratt-9999.py
@@ -0,0 +1,26 @@
+from package import *
+
+class ratt(MakePackage):
+    dependencies=['mummer']
+
+    def fetch(self):
+        runCommand('svn co https://ratt.svn.sourceforge.net/svnroot/ratt ratt')
+   
+    unpack=""
+    workdir='ratt'
+
+    config=""
+
+    build="""
+        mkdir -p %(prefix)s/opt/ratt
+        cp -R * %(prefix)s/opt/ratt
+        cd %(prefix)s/opt/ratt
+        """
+
+    install="""
+        cd %(prefix)s/opt/ratt
+        ln -f -s ../opt/ratt/start.ratt.sh %(prefix)s/bin/
+        cat %(rootpath)s/paths | grep -v RATT_HOME > temppath
+        echo "export RATT_HOME=%(prefix)s" >> temppath
+        cp temppath %(rootpath)s/paths
+    """ 
