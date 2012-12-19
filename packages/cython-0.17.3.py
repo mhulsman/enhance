ec8867a3a9fa4f2b454123e408513c31416565ae
Marc Hulsman <m.hulsman@tudelft.nl>
2012-12-19 22:38:35 +0100

    New packages

---
+++ packages/cython-0.17.3.py
@@ -0,0 +1,5 @@
+from package import *
+
+class cython(PythonPackage):
+    dependencies = ["python"]
+    fetch = "http://www.cython.org/release/Cython-0.17.3.tar.gz"
---
+++ packages/pysam-0.7.py
@@ -0,0 +1,6 @@
+from package import *
+
+class pysam(PythonPackage):
+    dependencies = ["cython","python"]
+    fetch = "http://pysam.googlecode.com/files/pysam-0.7.tar.gz"
+
