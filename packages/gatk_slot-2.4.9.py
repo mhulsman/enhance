a7f95d1bf6aa579c1e171a56c44460fc2ad3b1a5
Marc Hulsman <m.hulsman@tudelft.nl>
2013-06-25 13:40:56 +0200

    Add extra package vor older version of gatk

---
+++ packages/gatk_slot-2.4.9.py
@@ -0,0 +1,26 @@
+from package import *
+
+
+class gatk_slot(Package):
+    def fetch(self):
+        f = self.fillVars('%(srcpath)s/GenomeAnalysisTK-2.4-9.tar.bz2')
+        if not os.path.isfile(f):
+            error('Please supply source file: %s' %f)
+        return f
+    
+    workdir="GenomeAnalysisTK-2.4-9-g532efad"
+
+    build="""
+        mkdir -p %(prefix)s/opt/gatk-%(version)s
+        cp -R * %(prefix)s/opt/gatk-%(version)s
+        cd %(prefix)s/opt/gatk-%(version)s
+        """
+
+    install="""
+        cd %(prefix)s/opt/gatk-%(version)s
+        echo "#!/bin/bash" > gatk-%(version)s
+        echo "java -jar %(prefix)s/opt/gatk-%(version)s/GenomeAnalysisTK.jar \$@" >> gatk-%(version)s
+        chmod +x gatk-%(version)s
+        cp ./gatk-%(version)s %(prefix)s/bin/
+    """ 
+    
