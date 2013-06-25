b0dafa838cebcfdf3a70cf4e5dc982bc8dd0533b
Marc Hulsman <m.hulsman@tudelft.nl>
2013-06-25 14:22:12 +0200

    Added gsl, fix in dependency isl, added zlib

---
+++ packages/gsl-1.15.py
@@ -0,0 +1,4 @@
+from package import *
+class gsl(MakePackage):
+    #dependencies=['gmp','mpfr']
+    fetch='ftp://ftp.gnu.org/gnu/gsl/gsl-1.15.tar.gz'
--- packages/isl-0.11.py
+++ packages/isl-0.11.py
@@ -1,4 +1,5 @@
 from package import *
 
 class isl(MakePackage):
+    dependencies = ['gmp']
     fetch="ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/isl/isl-0.11.tar.gz"
