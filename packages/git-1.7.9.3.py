13321064d39350259ac98ed1ce9b49e0b3670120
Alexey Gritsenko <grizenko.a@gmail.com>
2013-11-29 01:24:48 +0100

    Updated packages

--- packages/git-1.7.9.3.py
+++ packages/git-1.7.9.3.py
@@ -5,5 +5,5 @@ class git(Package):
 
     fetch="http://git-core.googlecode.com/files/git-1.7.9.3.tar.gz"
 
-    install="make PYTHON_PATH=%(prefix)s/bin/python NO_EXPAT=YesPlease NO_TCLTK=YesPlease prefix=%(prefix)s all install"
+    install="make PYTHON_PATH=%(prefix)s/bin/python NO_EXPAT=YesPlease NO_TCLTK=YesPlease PERL_PATH=%(prefix)s/bin/perl prefix=%(prefix)s all install"
 
--- packages/isl-0.11.py
+++ packages/isl-0.11.py
@@ -2,4 +2,5 @@ from package import *
 
 class isl(MakePackage):
     dependencies = ['gmp']
-    fetch="ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/isl/isl-0.11.tar.gz"
+    fetch='ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.11.1.tar.bz2'
+    #fetch="ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/isl/isl-0.11.tar.gz"
