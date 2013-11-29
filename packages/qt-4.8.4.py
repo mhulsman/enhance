d37b049c22a2cd058c334c6de1fc355b67273c51
Alexey Gritsenko <grizenko.a@gmail.com>
2013-11-29 15:29:38 +0100

    Updated fetch URL. Compilation works now, but requires some textual input during configuration. Someone should

--- packages/qt-4.8.4.py
+++ packages/qt-4.8.4.py
@@ -2,4 +2,4 @@ from package import *
 
 class qt(MakePackage):
     dependencies=['gcc','xorg']
-    fetch="http://releases.qt-project.org/qt4/source/qt-everywhere-opensource-src-%(version)s.tar.gz"
+    fetch='http://pkgs.fedoraproject.org/repo/pkgs/qt/qt-everywhere-opensource-src-%(version)s.tar.gz/89c5ecba180cae74c66260ac732dc5cb/qt-everywhere-opensource-src-%(version)s.tar.gz'
