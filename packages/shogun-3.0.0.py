2f449a6e86470ee4658b004dc1d14cbe76f12538
Alexey Gritsenko <grizenko.a@gmail.com>
2013-12-03 10:19:55 +0100

    Added a new version of Shogun

---
+++ packages/shogun-3.0.0.py
@@ -0,0 +1,11 @@
+from package import *
+
+class shogun(MakePackage):
+  
+  dependencies=['numpy', 'atlas','lapack','R','perl','eigen','swig']
+  fetch = 'http://shogun-toolbox.org/archives/shogun/releases/3.0/sources/shogun-3.0.0.tar.bz2'
+
+  build_reldir = 'build'
+  config = 'cmake .. -DCMAKE_INSTALL_PREFIX=%(prefix)s -DCMAKE_BUILD_TYPE=Debug -DPythonModular=ON'
+
+  
