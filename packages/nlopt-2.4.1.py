278dde5815ebd1439451e77eb418eeb4d7b30a0f
Alexey Gritsenko <grizenko.a@gmail.com>
2013-12-17 17:07:13 +0100

    Added config options to create shared libraries

--- packages/nlopt-2.4.1.py
+++ packages/nlopt-2.4.1.py
@@ -1,6 +1,7 @@
 from package import *
 
 class nlopt(MakePackage):
+    config = './configure --prefix=%(prefix)s --enable-shared --without-octave --without-matlab'
     fetch="http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz"
 
     
