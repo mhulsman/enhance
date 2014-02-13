df24141396d6264672c2bc362960f50cc4c66279
Marc Hulsman <m.hulsman@tudelft.nl>
2014-02-13 09:52:41 +0100

    New packages

---
+++ packages/igraph-0.6.5.py
@@ -0,0 +1,4 @@
+from package import *
+
+class igraph(MakePackage):
+    fetch="http://pkgs.fedoraproject.org/repo/pkgs/igraph/igraph-0.6.5.tar.gz/5f9562263ba78b31c564d6897ff5a110/igraph-0.6.5.tar.gz"
