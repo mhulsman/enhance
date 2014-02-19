2945397088f10ef6d8e86252e4107cef8ce9c636
Alexey Gritsenko <grizenko.a@gmail.com>
2014-02-19 21:43:12 +0100

    Updated libzqm

--- packages/zeromq-4.0.3.py
+++ packages/zeromq-4.0.3.py
@@ -2,6 +2,6 @@ from package import *
 
 class zeromq(MakePackage):
     dependencies=['e2fsprogs']
-    fetch="http://download.zeromq.org/zeromq-2.1.11.tar.gz"
+    fetch="http://download.zeromq.org/zeromq-4.0.3.tar.gz"
     
     config="./configure --prefix=%(prefix)s"
