110b1f78904a26144acebfe0ca3f0d4ff786adf7
Alexey Gritsenko <grizenko.a@gmail.com>
2014-02-27 10:03:15 +0100

    Updated gperftools

--- packages/gperftools-2.1.py
+++ packages/gperftools-2.1.py
@@ -2,6 +2,6 @@ from package import *
 
 class gperftools(MakePackage):
     dependencies = ['libunwind']
-    fetch="http://gperftools.googlecode.com/files/gperftools-2.0.tar.gz"
+    fetch="http://gperftools.googlecode.com/files/gperftools-2.1.tar.gz"
 
 
