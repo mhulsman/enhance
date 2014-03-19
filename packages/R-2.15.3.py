536f447174df5fccc431307a527edb97a2da6cd7
Alexey Gritsenko <grizenko.a@gmail.com>
2014-03-19 09:51:47 +0100

    R as a shared library

--- packages/R-2.15.3.py
+++ packages/R-2.15.3.py
@@ -3,5 +3,5 @@ from package import *
 class R(MakePackage):
     dependencies = ["tcl","tk",'xorg']
     fetch="http://cran.xl-mirror.nl/src/base/R-2/R-2.15.3.tar.gz"
-    config = "./configure --prefix=%(prefix)s --with-tcl-config=%(prefix)s/lib64/tclConfig.sh --with-tk-config=%(prefix)s/lib64/tkConfig.sh"
+    config = "./configure --prefix=%(prefix)s --with-tcl-config=%(prefix)s/lib64/tclConfig.sh --with-tk-config=%(prefix)s/lib64/tkConfig.sh --enable-R-shlib"
 
