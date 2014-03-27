b95e70d9f8677fa3d0894f72563c8891f3662875
Alexey Gritsenko <grizenko.a@gmail.com>
2014-03-27 18:45:37 +0100

    Added new zeromq

---
+++ packages/zeromq-4.0.4.py
@@ -0,0 +1,7 @@
+from package import *
+
+class zeromq(MakePackage):
+    dependencies=['e2fsprogs']
+    fetch="http://download.zeromq.org/zeromq-4.0.4.tar.gz"
+    
+    config="./configure --prefix=%(prefix)s"
