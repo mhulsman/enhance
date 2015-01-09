6af3c2608962d24fdc56605cc7e641070edfc41a
Marc Hulsman <m.hulsman@tudelft.nl>
2015-01-09 15:53:17 +0100

    Add patch to samtools

--- packages/samtools-1.1.py
+++ packages/samtools-1.1.py
@@ -4,7 +4,11 @@ class samtools(Package):
     fetch="http://downloads.sourceforge.net/project/samtools/samtools/1.1/samtools-1.1.tar.bz2"
     dependencies = ['zlib','ncurses']
     build="make"
-    
+
+    config = """
+    patch -p0 < %(distpath)s/packages/samtools_1.1_stats.patch
+    """
+
     install="""
     make prefix=%(prefix)s install
     """
