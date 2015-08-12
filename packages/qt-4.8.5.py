670dc711f1ab03500ac1e251d14d48f591a94610
Jasper Linthorst <jasper.linthorst@gmail.com>
2015-08-12 10:35:03 +0200

    Update url and configure to accept opensource license[D

--- packages/qt-4.8.5.py
+++ packages/qt-4.8.5.py
@@ -2,4 +2,5 @@ from package import *
 
 class qt(MakePackage):
     dependencies=['gcc','xorg']
-    fetch='http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-everywhere-opensource-src-%(version)s.tar.gz'
+    fetch='https://download.qt.io/archive/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz'
+    config='./configure -opensource -confirm-license'
